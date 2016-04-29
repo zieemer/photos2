3395e93a58de5a84a0f7b69772e05fc822288737
sylwester_z <Sylwester.Ziemer@gap.uk.com>
2016-04-29 12:46:42 +0100

    Start

---
+++ www/js/controllers.js
@@ -0,0 +1,32 @@
+angular.module('app.controllers', [])
+  
+.controller('welcomeCtrl', function($scope,$cordovaCamera) {
+     $scope.ready = false;
+     
+ document.addEventListener('deviceready', function () {
+     $scope.ready = true;
+     $scope.device = $cordovaDevice.getDevice();
+     $scope.takeImage = function() {
+     console.log(Camera)
+        var options = {
+            quality: 80,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 250,
+            targetHeight: 250,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+         
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.srcImage = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // error
+        });
+    }
+ });
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,32 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+  
+      
+        
+    .state('welcome', {
+      url: '/',
+      templateUrl: 'templates/welcome.html',
+      controller:'welcomeCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  
+  $urlRouterProvider.otherwise('/');
+  
+
+  
+
+});
